2ac594f600237ad1b43fecaf621366eb2c71c93b
ursuswork <ursuswork84@gmail.com>
2025-07-14 17:59:57 -0600

    componentes completos

--- conexion.php
+++ conexion.php
@@ -1,9 +1,8 @@
 <?php
-$host = getenv("MYSQL_ADDON_HOST");
-$user = getenv("MYSQL_ADDON_USER");
-$pass = getenv("MYSQL_ADDON_PASSWORD");
-$db   = getenv("MYSQL_ADDON_DB");
-
+$host = "b3p6qq8wsihgfuawhkmt-mysql.services.clever-cloud.com";
+$user = "u0f3xgk6ntxayxon";
+$pass = "040ii4QwsywMuZIQqLHM";
+$db = "b3p6qq8wsihgfuawhkmt";
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
