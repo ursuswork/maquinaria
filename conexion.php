517087a687411551c05b347cd4c4b6c09485f601
ursuswork <ursuswork84@gmail.com>
2025-07-14 15:21:33 -0600

    datos conexion

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "inventario_maquinaria";
+$host = "b3p6qq8wsihgfuawhkmt-mysql.services.clever-cloud.com";
+$user = "u0f3xgk6ntxayxon";
+$pass = "040ii4QwsywMuZIQqLHM";
+$db = "b3p6qq8wsihgfuawhkmt";
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
