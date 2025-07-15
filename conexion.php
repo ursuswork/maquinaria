4afe6154de3011bc5a954893ae7fe617667d67c8
ursuswork <ursuswork84@gmail.com>
2025-07-15 15:54:40 -0600

    modificaciones

--- conexion.php
+++ conexion.php
@@ -1,10 +1,1 @@
-<?php
-$host = "b3p6qq8wsihgfuawhkmt-mysql.services.clever-cloud.com";
-$user = "u0f3xgk6ntxayxon";
-$pass = "040ii4QwsywMuZIQqLHM";
-$db = "b3p6qq8wsihgfuawhkmt";
-$conn = new mysqli($host, $user, $pass, $db);
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
-?>
+<?php $conn = new mysqli('localhost', 'user', 'pass', 'db'); ?>
