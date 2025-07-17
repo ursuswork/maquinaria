bf924da1eea9493c7326497ad96ab9fbb4cfaf85
ursuswork <ursuswork84@gmail.com>
2025-07-17 00:43:32 -0600

    login

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-echo "✅ index.php está funcionando correctamente.";
+session_start();
+
+// Si el usuario ya inició sesión, lo envías al inventario
+if (isset($_SESSION['usuario'])) {
+  header("Location: inventario.php");
+  exit;
+}
+
+// Si no ha iniciado sesión, lo envías al login
+header("Location: login.php");
+exit;
 ?>
