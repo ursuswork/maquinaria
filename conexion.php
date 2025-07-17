be6ca9aa7947ab6bbd93403637296171c84f544b
ursuswork <ursuswork84@gmail.com>
2025-07-16 23:15:26 -0600

    3

--- conexion.php
+++ conexion.php
@@ -1,12 +1,14 @@
 <?php
-// CONFIGURA ESTOS DATOS PARA CLEVER CLOUD
+// Conexión a MySQL en Clever Cloud
 $servername = "b3p6qq8wsihgfuawhkmt-mysql.services.clever-cloud.com";
-$username = "u0f3xgk6ntxayxon";
-$password = "040ii4QwsywMuZIQqLHM";
-$dbname = "b3p6qq8wsihgfuawhkmt";
-$puerto = 3306;
+$username   = "u0f3xgk6ntxayxon";
+$password   = "040ii4QwsywMuZIQqLHM";
+$dbname     = "b3p6qq8wsihgfuawhkmt";
+$puerto     = 3306;
 
 $conn = new mysqli($servername, $username, $password, $dbname, $puerto);
+
+// Verificar conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
