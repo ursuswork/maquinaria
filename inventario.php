db64950627e189c55e8022e23c965347e0fb1ecc
ursuswork <ursuswork84@gmail.com>
2025-07-25 15:49:11 -0600

    3

--- inventario.php
+++ inventario.php
@@ -81,7 +81,6 @@ $resultado = $conn->query($sql);
 if (($tipo_filtro === 'produccion nueva' || $tipo_filtro === 'nueva') && $subtipo_filtro !== 'todos') {
     $sql .= " AND LOWER(TRIM(m.subtipo)) = '" . $conn->real_escape_string($subtipo_filtro) . "'";
 }
-
   <ul class="nav nav-pills mb-3 ms-3">
   <li class="nav-item">
     <a class="nav-link <?= $subtipo_filtro==='todos'?'active':''?>" href="?tipo=produccion nueva&subtipo=todos">Todos</a>
