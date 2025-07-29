e0f476d821428935dd621ec2b378fa6ae4fb56f1
ursuswork <ursuswork84@gmail.com>
2025-07-29 17:53:28 -0600

    4

--- calculo_avance_por_subtipo.php
+++ calculo_avance_por_subtipo.php
@@ -2,17 +2,21 @@
 $avance = 0;
 
 if ($subtipo === 'bachadora') {
-  $sql_avance = "SELECT etapa, peso FROM avance_bachadora WHERE id_maquinaria = $id";
+  $query = "SELECT peso FROM avance_bachadora WHERE id_maquinaria = $id";
 } elseif ($subtipo === 'esparcidor de sello') {
-  $sql_avance = "SELECT etapa, peso FROM avance_esparcidor WHERE id_maquinaria = $id";
+  $query = "SELECT peso FROM avance_esparcidor WHERE id_maquinaria = $id";
 } elseif ($subtipo === 'petrolizadora') {
-  $sql_avance = "SELECT etapa, peso FROM avance_petrolizadora WHERE id_maquinaria = $id";
+  $query = "SELECT peso FROM avance_petrolizadora WHERE id_maquinaria = $id";
+} else {
+  $query = null;
 }
 
-if (isset($sql_avance)) {
-  $res_avance = $conn->query($sql_avance);
-  while ($fila_av = $res_avance->fetch_assoc()) {
-    $avance += intval($fila_av['peso']);
+if ($query) {
+  $resultado_avance = $conn->query($query);
+  if ($resultado_avance) {
+    while ($etapa = $resultado_avance->fetch_assoc()) {
+      $avance += intval($etapa['peso']);
+    }
   }
 }
 ?>
