9879dea60b596276c205016e6afbf0c6e1f85ef3
ursuswork <ursuswork84@gmail.com>
2025-08-06 16:29:33 -0600

    121

--- avance_petrolizadora.php
+++ avance_petrolizadora.php
@@ -133,7 +133,7 @@ $fecha_actualizacion = $conn->query("SELECT updated_at FROM avance_petrolizadora
     h3, h5 {
       color: #ffc107;
       text-align: center;
-      font-size: 1.6rem;
+      font-size: 1.4rem;
     }
     .progress {
       height: 35px;
